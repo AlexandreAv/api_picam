166b7411b2a6c0815fce8e256e0f765a70b47ec3
Alexandre Rangom <alexandre.rangom@gmail.com>
2020-05-02 18:03:14 +0200

    Ajouts de requêtes et en conséquence de tests unitaires

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -5,8 +5,8 @@ const { connection } = require('../datasources/mysql.connector');
 const queryPromise = promisify(connection.query).bind(connection);
 
 /* Create user */
-const createUserSql = `INSERT INTO T_USER (firstname, lastname, nickname, pass, email)
-                         VALUES ('??', '??', '??', '??', '??')`;
+const createUserSql = `INSERT INTO T_USER (firstname, lastname, nickname, hash, email)
+                         VALUES (?, ?, ?, ?, ?)`;
 
 function postUserQuery(data) {
     return queryPromise({
@@ -16,7 +16,7 @@ function postUserQuery(data) {
 }
 
 /* Find user by id */
-const findUserByIdSql = 'SELECT * FROM T_USER WHERE id= ??';
+const findUserByIdSql = 'SELECT * FROM T_USER WHERE id=?';
 
 function findUserById(id) {
     return queryPromise({
@@ -25,7 +25,29 @@ function findUserById(id) {
     });
 }
 
+/* Find user by id */
+const findHashByEmailSql = 'SELECT hash FROM T_USER WHERE email=?';
+
+function findHashByEmail(email) {
+    return queryPromise({
+        sql: mysql.format(findHashByEmailSql, email),
+        timeout: 10000,
+    });
+}
+
+/* Find user by id */
+const findIdByEmailSql = 'SELECT id FROM T_USER WHERE email=?';
+
+function findIdByEmail(email) {
+    return queryPromise({
+        sql: mysql.format(findIdByEmailSql, email),
+        timeout: 10000,
+    });
+}
+
 module.exports = {
     postUserQuery,
     findUserById,
+    findHashByEmail,
+    findIdByEmail,
 };
--- test/services/user.services.test.js
+++ test/services/user.services.test.js
@@ -1,23 +1,45 @@
 const {
     postUserQuery,
     findUserById,
+    findHashByEmail,
+    findIdByEmail,
 } = require('../../src/services/user.services.js');
 
-describe('#postUserQuery()', () => {
+describe('Teste des requêtes envoyer à la base de donnée pour le controller user', () => {
     context("Test de création d'user sur la base de donnée", () => {
         it('Requête insertion de données à la table user', () =>
-            postUserQuery(['nom', 'prénom', 'surnom', 'pass', 'email'])
+            postUserQuery([
+                'nom',
+                'prénom',
+                'surnom',
+                'pass',
+                'bonjour.test@gmail.com',
+            ])
                 .then((result, field) => {})
                 .catch((err) => {
                     throw err;
                 }));
     });
-});
 
-describe('#findUserById()', () => {
     context('Trouver un utilisateur par son id', () => {
         it("requête de sélection d'élément dans la table user par son id", () =>
-            postUserQuery(1)
+            findUserById(5)
+                .then((result, field) => {})
+                .catch((err) => {
+                    throw err;
+                }));
+    });
+
+    context('Trouver un utilisateur par son email', () => {
+        it('requête de sélection du hash dans la table user par son email', () =>
+            findHashByEmail('email')
+                .then((result, field) => {})
+                .catch((err) => {
+                    throw err;
+                }));
+
+        it('requête de sélection du id dans la table user par son email', () =>
+            findIdByEmail('rrangom@gmail.com')
                 .then((result, field) => {})
                 .catch((err) => {
                     throw err;
